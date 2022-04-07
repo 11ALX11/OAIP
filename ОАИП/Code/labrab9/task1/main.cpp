0a44f9b45e8df68e89049ccea1b5c59b63a6d357
Aleksey <11_alx_11@ro.ru>
2022-04-07 21:55:46 +0300

    labrab9 code

--- ОАИП/Code/labrab9/task1/main.cpp
+++ ОАИП/Code/labrab9/task1/main.cpp
@@ -1,40 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void NMinmax(float* A, int& N, int& NMin, int& NMax);
+void NMinmax(float* A, int* N, int& NMin, int& NMax);
 
 int main() {
 
     int NA=0, NB=0, NC=0, NMin, NMax;
-    int &arrOfNs[3] = {NA, NB, NC};
+    int* arrOfNs[3] = {&NA, &NB, &NC};
 
     scanf("%d %d %d", &NA, &NB, &NC);
 
     float A[NA];
     float B[NB];
     float C[NC];
-    float** arrOfArrs = {A, B, C};
+    float* arrOfArrs[3] = {A, B, C};
 
     for (int j=0; j<3; j++) {
-        for (int i=0; i<=arrOfNs[j]; i++) scanf("%f", &(arrOfArrs[j][i]));
+        for (int i=0; i<*(arrOfNs[j]); i++) scanf("%f", &(arrOfArrs[j][i]));
     }
 
     for (int i=0; i<3; i++) {
-        NMinmax(arrOfArrs[i], &NA, &NMin, &NMax);
-        printf("%d %d\n", NMin, NMax);
+        NMinmax(arrOfArrs[i], arrOfNs[i], NMin, NMax);
+        printf("%d %d\n", NMin+1, NMax+1);
     }
-    NMinmax(B, NB, &NMin, &NMax);
-    printf("%d %d\n", NMin, NMax);
-    NMinmax(C, NC, &NMin, &NMax);
-    printf("%d %d\n", NMin, NMax);
 
     return 0;
 }
 
-void NMinmax(float* A, int& N, int& NMin, int& NMax) {
-    NMin=1;
-    NMax=1;
-    for (int i=1; i<=N; i++) {
+void NMinmax(float* A, int* N, int& NMin, int& NMax) {
+    NMin=0;
+    NMax=0;
+    for (int i=0; i<*N; i++) {
         if (A[NMin] > A[i]) NMin=i;
         if (A[NMax] < A[i]) NMax=i;
     }
